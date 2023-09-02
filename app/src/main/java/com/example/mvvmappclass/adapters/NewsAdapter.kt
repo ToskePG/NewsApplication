cd903cefde89b4931c2074e4f6ef6c9b730872a3
ToskePG <danilo.tosic.pmf@gmail.com>
2023-09-02 22:46:54 +0200

    Handles null image in adapter.

--- app/src/main/java/com/example/mvvmappclass/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/mvvmappclass/adapters/NewsAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.mvvmappclass.R
 import com.example.mvvmappclass.databinding.ItemArticlePreviewBinding
 import com.example.mvvmappclass.model.Article
 
@@ -38,14 +39,18 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
         val article = differ.currentList[position]
-        holder.binding.apply {
-            Glide.with(ivArticleImage.context).load(article.urlToImage).into(ivArticleImage)
-            tvSource.text = article.source?.name
-            tvTitle.text = article.title
-            tvDescription.text = article.description
-            tvPublishedAt.text = article.publishedAt
-            holder.itemView.setOnClickListener {
-                onItemClickListener?.let { it(article) }
+        if(article.urlToImage == null){
+            holder.binding.ivArticleImage.setImageResource(R.drawable.news)
+        }else{
+            holder.binding.apply {
+                Glide.with(ivArticleImage.context).load(article.urlToImage).into(ivArticleImage)
+                tvSource.text = article.source?.name
+                tvTitle.text = article.title
+                tvDescription.text = article.description
+                tvPublishedAt.text = article.publishedAt
+                holder.itemView.setOnClickListener {
+                    onItemClickListener?.let { it(article) }
+                }
             }
         }
     }
@@ -55,8 +60,7 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
     }
 
     private var onItemClickListener: ((Article) -> Unit)? = null
-
-
+    
     fun setOnItemClickListener(listener: (Article) -> Unit) {
         onItemClickListener = listener
     }
